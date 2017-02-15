1edba80d148373655193165fc35f56ffa8151950
Stéphane Klein <contact@stephane-klein.info>
2017-02-15 22:19:41 +0100

    Add client https support

--- copy_certs.sh
+++ copy_certs.sh
@@ -3,6 +3,7 @@
 mkdir -p src/etcd0/certs
 mkdir -p src/etcd1/certs
 mkdir -p src/etcd2/certs
+mkdir -p src/etcdctl/certs
 
 cp src/etcd0/tls-setup/certs/ca* src/etcd0/certs/
 cp src/etcd0/tls-setup/certs/etcd0* src/etcd0/certs/
@@ -12,3 +13,6 @@ cp src/etcd0/tls-setup/certs/etcd1* src/etcd1/certs/
 
 cp src/etcd0/tls-setup/certs/ca* src/etcd2/certs/
 cp src/etcd0/tls-setup/certs/etcd2* src/etcd2/certs/
+
+cp src/etcd0/tls-setup/certs/ca* src/etcdctl/certs/
+cp src/etcd0/tls-setup/certs/etcdctl* src/etcdctl/certs/
